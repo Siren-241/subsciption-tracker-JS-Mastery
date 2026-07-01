9ffe6e8253be96b11b7508e0098f41a2749f5e57
Siren-241 <ovalekarsaksham7@gmail.com>
2026-07-01 12:15:21 +0530

    Added Arcjet (rate-limiting, bot-prevention) middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
--- config/env.js
+++ config/env.js
@@ -10,5 +10,7 @@ export const {
 	DB_URI,
 	JWT_SECRET,
 	JWT_EXPIRES_IN,
+	ARCJET_KEY,
+	ARCJET_ENV,
 
 } = process.env;
